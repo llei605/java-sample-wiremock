bece465b002d045a51a0e00740ec035e6ca1508c
llei <llei605@qq.com>
2018-12-19 00:27:54 +0800

    2018-12-19 00:27:33

--- javaDemo/wiremockDemo/src/main/java/com/sample/MockServer.java
+++ javaDemo/wiremockDemo/src/main/java/com/sample/MockServer.java
@@ -1,6 +1,10 @@
 package com.sample;
 
+import com.github.tomakehurst.wiremock.client.MappingBuilder;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.matching.StringValuePattern;
+import com.github.tomakehurst.wiremock.matching.UrlPattern;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.io.ClassPathResource;
@@ -10,6 +14,9 @@ import java.io.IOException;
 public class MockServer {
 
     public static void main(String[] args) {
+
+        // step 0. 清空之前的配置
+        // WireMock.removeAllMappings();
 
         // step 1. 告诉程序： mock服务器在哪里
         WireMock.configureFor(8060); // 由于Wiremock架在本地，因此只需要输入端口号即可
@@ -30,13 +37,57 @@ public class MockServer {
             e.printStackTrace();
         }
 
+        // step 4. 模拟登陆验证
+        try {
+            loginSuccess();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e.toString());
+        }
+        try {
+            loginFaild();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e.toString());
+        }
+
     }
 
     private static void retFileContent(String url, String fileName) throws IOException {
 
-        ClassPathResource classPathResource = new ClassPathResource("mock/response" + fileName + ".txt");
+        ClassPathResource classPathResource = new ClassPathResource("mock/response/" + fileName + ".txt");
         String retContent = StringUtils.join(FileUtils.readLines(classPathResource.getFile(), "UTF-8").toArray(), "\n");
-        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo(url)).willReturn(WireMock.aResponse().withBody(retContent).withStatus(200)));
+        // WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo(url)).willReturn(WireMock.aResponse().withBody(retContent).withStatus(200)));
+        WireMock.stubFor(WireMock.request("GET", WireMock.urlPathEqualTo(url)).willReturn(WireMock.aResponse().withBody(retContent).withStatus(200)));
+    }
+
+    private static void loginSuccess() throws IOException {
+
+        UrlPattern urlPattern = WireMock.urlEqualTo("/login");
+        StringValuePattern bodyPattern = WireMock.equalToJson("{\"username\": \"aaa\", \"password\": \"111111\"}");
+        MappingBuilder mappingBuilder = WireMock.request("POST", urlPattern).withRequestBody(bodyPattern);
+
+        ClassPathResource classPathResource = new ClassPathResource("mock/response/login/success.txt");
+        String retContent = StringUtils.join(FileUtils.readLines(classPathResource.getFile(), "UTF-8").toArray(), "\n");
+        
+        WireMock.stubFor(WireMock.post(WireMock.urlPathEqualTo("/login"))
+            .withRequestBody(bodyPattern)
+            .willReturn(WireMock.aResponse().withBody(retContent).withStatus(200)));
+
+    }
+
+    private static void loginFaild() throws IOException {
+
+        UrlPattern urlPattern = WireMock.urlEqualTo("/login");
+        StringValuePattern bodyPattern = WireMock.equalToJson("{\"username\": \"bbb\", \"password\": \"111111\"}");
+        MappingBuilder mappingBuilder = WireMock.request("POST", urlPattern).withRequestBody(bodyPattern);
+
+        ClassPathResource classPathResource = new ClassPathResource("mock/response/login/faild.txt");
+        String retContent = StringUtils.join(FileUtils.readLines(classPathResource.getFile(), "UTF-8").toArray(), "\n");
+        
+        WireMock.stubFor(WireMock.post(WireMock.urlPathEqualTo("/login"))
+            .withRequestBody(bodyPattern)
+            .willReturn(WireMock.aResponse().withBody(retContent).withStatus(200)));
 
     }
 }
